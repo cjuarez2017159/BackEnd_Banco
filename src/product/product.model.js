9a131450abcbac99173ac2b7fce69826e4554761
darriola2021670 <darriola-2021670@kinal.edu.gt>
2024-05-28 16:02:16 -0600

    se creo el modelo de products

---
+++ src/product/product.model.js
@@ -0,0 +1,16 @@
+import mongose from "mongoose"
+
+const ProductSchema = mongose.Schema({
+    products: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+});
+
+const product = model ("Product", ProductSchema);
+
+export default product;
