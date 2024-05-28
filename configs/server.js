251abba572784521f961ffe4790f29d5b8b52a3f
arodriguez2019473 <arodriguez-2019473@kinal.edu.gt>
2024-05-28 13:10:02 -0600

    se crea las bases de backend

---
+++ configs/server.js
@@ -0,0 +1,45 @@
+'use strict'
+import bcryptjs from 'bcryptjs';
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import { dbConnection } from './mongo.js'
+
+
+
+class Server{
+
+    constructor(){
+        this.app = express();
+        this.port = process.env.PORT;
+
+        this.conectarDB();
+        this.middlewares();
+        this.routes();
+        
+    }
+
+    async conectarDB(){
+        await dbConnection();
+    }
+
+    middlewares(){
+        this.app.use(express.static('public'));
+        this.app.use(cors());
+        this.app.use(express.json());
+    }
+
+    routes(){
+
+    }
+
+    listen(){
+        this.app.listen(this.port, ()=>{
+            console.log('Server running and listening to port', this.port)
+        })
+    }
+
+}
+
+export default Server;
