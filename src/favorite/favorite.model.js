b607c9010008485fc594d74d4422721715c7ca14
darriola2021670 <darriola-2021670@kinal.edu.gt>
2024-06-05 16:04:03 -0600

    se agrego el modelo favorite

---
+++ src/favorite/favorite.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const FavoriteSchema = mongoose({
+    account_number: {
+        type: String,
+        required: true
+    },
+    alias:{
+        type: String,
+        required: true
+    },
+    DPI:{
+        type: String,
+        required: true
+    },
+    estado: {
+        type: String,
+        default: true
+    }
+});
+
+export default mongoose.model('Favorite', FavoriteSchema)
