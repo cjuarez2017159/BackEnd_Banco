9878dcc7d975f6b04743d9b6d31e9706b6cb14b1
unknown <arodriguez-2019473@kinal.edu.gt>
2024-07-03 14:05:35 -0600

    se hace historu

--- src/history/history.controller.js
+++ src/history/history.controller.js
@@ -6,11 +6,12 @@ export const HistoryGet = async (req, res) => {
     const query = { estado: true };
 
     try {
-      
         const [total, historias] = await Promise.all([
-      
             History.countDocuments(query),
-            History .find(query)
+            History.find(query)
+                .populate('account')
+                .populate('service')
+                .populate('product')
                 .skip(Number(desde))
                 .limit(Number(limite))
         ]);
@@ -18,23 +19,30 @@ export const HistoryGet = async (req, res) => {
         res.status(200).json({
             total,
             historias
-      
         });
-
-    } 
-    catch (error) {
+    } catch (error) {
         console.error('Error al obtener historias:', error);
         res.status(500).json({ error: 'Error interno del servidor' });
     }
 }
 
 export const getHistoryById = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const historia = await History.findById(id)
+            .populate('account')
+            .populate('service')
+            .populate('product');
 
-    const {id} = req.params;
-    const historia = await History.findOne({_id: id}); 
+        if (!historia) {
+            return res.status(404).json({ error: 'Historia no encontrada' });
+        }
 
-    res.status(200).json({
-        historia
-    })
-
+        res.status(200).json({
+            historia
+        });
+    } catch (error) {
+        console.error('Error al obtener la historia:', error);
+        res.status(500).json({ error: 'Error interno del servidor' });
+    }
 };
--- src/history/history.model.js
+++ src/history/history.model.js
@@ -1,38 +1,31 @@
 import mongoose from "mongoose";
-import account from "../account/account.model.js";
 
 const HistorySchema = mongoose.Schema({
-
     estado: {
         type: String,
         default: true,
     },
-
-
     account: {
         type: [{
-            amountAccount: { type: String, require: true},
-            account_number: { type: String, require: true},
-        
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Account'
         }],
         _id: false,
     },
-
-    service:{
+    service: {
         type: [{
-            service: { type: String, require: true},
-    }],
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Service'
+        }],
         _id: false,
     },
-
-    product:{
+    product: {
         type: [{
-            product: { type: String, require: true},
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Product'
         }],
         _id: false,
     }
-
-    
-})
+});
 
 export default mongoose.model('History', HistorySchema);
